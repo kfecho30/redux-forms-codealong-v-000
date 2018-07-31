e639decb48b786fe9f94652815105d3678928fe4
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:17:10 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,7 +1,8 @@
 export default function manageTodo(state = {todos: []}, action) {
   switch(action.type) {
-
-
-    return state;
+    case 'ADD_TODO':
+      return {todos: state.todos.concat(action.payload.text)}
+    default:
+      return state;
   }
 }
