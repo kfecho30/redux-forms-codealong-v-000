92dbcd0b0594c705a811ca977a26a7461a04b2ca
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 19:03:26 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+  
   render() {
     return(
       <div>
